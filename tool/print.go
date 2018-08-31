cfdda9b4bfd7c1be5b7b023be0dae4d796529c35
Terry.FromMac <tkstorm@163.com>
2018-08-31 20:58:49 +0800

    修改一个未检测的BUG

--- tool/print.go
+++ tool/print.go
@@ -1,26 +1,27 @@
 package tool
 
 import (
+	"fmt"
 	"log"
 )
 
 //error log
 func ErrorLog(err error, msg string) error {
 	if err != nil {
-		log.Printf("[x] %s: %s", err, msg)
+		fmt.Printf("[x] %s: %s", err, msg)
 	}
 	return err
 }
 
 //error log
 func ErrorLogP(msg string) {
-	log.Println("[x]", msg)
+	fmt.Println("[x]", msg)
 }
 
 //error log
 func ErrorLogf(err error, format string, v ...interface{}) error {
 	if err != nil {
-		log.Printf("[x] %s "+format, err, v)
+		fmt.Printf("[x] %s "+format, err, v)
 	}
 	return err
 }
@@ -28,15 +29,15 @@ func ErrorLogf(err error, format string, v ...interface{}) error {
 //fatal log
 func ErrorPanic(err error, msg string) {
 	if err != nil {
-		log.Fatalf("[x] %s: %s", err, msg)
+		log.Panicf("[x] %s: %s", err, msg)
 	}
 }
 
 //pretty log
 func PrettyPrint(v ...interface{}) {
-	log.Printf("[*] %+v", v)
+	fmt.Printf("[*] %+v", v)
 }
 
 func PrettyPrintf(format string, v ...interface{}) {
-	log.Printf("[*] "+format, v)
+	fmt.Printf("[*] "+format, v)
 }
