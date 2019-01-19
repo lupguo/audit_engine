f11e0755c785728227397ca59530564e2b3596d9
Terry.FromMac <tkstorm@163.com>
2019-01-19 10:44:30 +0800

    fix import cycle not allowed

--- audit-engine.go
+++ audit-engine.go
@@ -5,7 +5,6 @@ import (
 	"github.com/tkstorm/audit_engine/mydb"
 	"github.com/tkstorm/audit_engine/rabbit"
 	"github.com/tkstorm/audit_engine/task"
-	"github.com/tkstorm/audit_engine/tool"
 	"log"
 )
 
@@ -51,7 +50,7 @@ func running() {
 	switch {
 	case cmd.Pub:
 		log.Println("message publish to queue:", q.Name)
-		mq.Publish(q.Name, tool.SimulateData(q.Name, &cmd), cmd.RepNumber)
+		mq.Publish(q.Name, SimulateData(q.Name, &cmd), cmd.RepNumber)
 	case cmd.Cus:
 		log.Println("message consume from queue:", q.Name)
 		mq.Consume(q.Name, tk.GetWork(q.Name, cmd.T), cmd.NoAck)
@@ -60,4 +59,22 @@ func running() {
 	}
 }
 
+// select queue and prepare message data
+func SimulateData(qn string, cmd *config.CmdArgs) []byte {
+	if cmd.MsgData != "" {
+		return []byte(cmd.MsgData)
+	}
+	switch qn {
+	case config.QueName["SOA_AUDIT_BACK_MSG"]:
+		return rabbit.SimAuditBackMsg()
+	case config.QueName["SOA_AUDIT_MSG"]:
+		return rabbit.SimAuditMsg()
+	case config.QueName["OBS_RULE_CHANGE_MSG"]:
+		return []byte(`{"action":"upd|del|add","templete_id":1}`)
+	case config.QueName["OBS_PERSON_AUDIT_RESULT"]:
+		return []byte(`{"message_id":1,"status": 2}`)
+	default:
+		return []byte("Heyman, Cool")
+	}
+}
 
